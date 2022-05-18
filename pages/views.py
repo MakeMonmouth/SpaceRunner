79631990ccb5efbb08bd5ab3c80adfcf88c344a4
Matthew Macdonald-Wallace <matt@doics.co>
2022-05-18 19:43:56 +0100

    Add component status to home page for members

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from accounts import models as account_models
+from stockkeeper import models as sk_models
 
 class HomeView(TemplateView):
     template_name = "pages/home.html"
@@ -9,7 +10,18 @@ class HomeView(TemplateView):
         context = super(HomeView, self).get_context_data(**kwargs)
         user = context['view'].request.user
         if user.is_authenticated:
-            print("User is authenticated")
+            context['user'] = user
+            component_types = []
+            for ct in sk_models.ComponentType.objects.all().order_by("name"):
+                if ct.component_set.count() > 0:
+                    component_types.append(
+                            {
+                                "id": ct.id,
+                                "name": ct.name,
+                                "qty": ct.component_set.count()
+                                }
+                            )
+            context['component_types'] = component_types
         else:
             context['memberships'] = account_models.MembershipType.objects.select_related()
         return context
